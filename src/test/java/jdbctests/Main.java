2f63acf72a794133fc3d1d0b78d25db45247e146
gokhanguz <67508424+gokhanguz@users.noreply.github.com>
2020-11-08 22:09:08 +0300

    Jdbc

---
+++ src/test/java/jdbctests/Main.java
@@ -0,0 +1,11 @@
+package jdbctests;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String dbUrl = "jdbc:oracle:thin:@3.85.237.76:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+    }
+}
